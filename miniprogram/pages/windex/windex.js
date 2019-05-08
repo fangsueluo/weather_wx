607ce55759fafb33de71d5e6ea4198dead4c51dc
yanfang.luo <yanfang.luo@wenba100.com>
2019-05-08 16:23:42 +0800

    canvas 折线绘制 第一阶段

--- miniprogram/pages/windex/windex.js
+++ miniprogram/pages/windex/windex.js
@@ -8,15 +8,18 @@ Page({
     city: '',
     location: '',
     province: '',
-    liveData: {},
-    humidity: {},
+    humidity: '',
     curTemperature: '',
     condTxt: '',
     windDirection: '',
     windPower: '',
     airQualityIndex: '',
     airQuality: '',
-    lifeIndexArr: []
+    lifeIndexArr: [],
+    forecastData: [],
+    hourlyData: [],
+    cWidth: 100,
+    cHeight: 50
   },
 
   /**
@@ -26,6 +29,7 @@ Page({
     this.getWeatherInfo()
     this.getWeatherInfo('forecast')
     this.getWeatherInfo('lifestyle')
+    this.getWeatherInfo('hourly')
     this.getAirInfo()
   },
 
@@ -101,12 +105,24 @@ Page({
           switch(type) {
             case 'now':
               this.setNowWeather(weather);
-              return
+              return;
             case 'lifestyle':
               this.setData({
                 lifeIndexArr: weather.lifestyle
               })
-              return
+              return;
+            case 'hourly':
+              this.setData({
+                hourlyData: []
+              })
+              return;
+            case 'forecast':
+              this.setData({
+                forecastData: weather.daily_forecast
+              });
+              this.createCanvas('tmpMax', 'tmp_max', "#e78f44");
+              this.createCanvas('tmpMin', 'tmp_min', '#5db8e3', 0);
+              return;
           }
         }
       },
@@ -136,5 +152,57 @@ Page({
         }
       }
     })
+  },
+  createCanvas(canvas, key, color, textUp=1) {
+    const ctx = wx.createCanvasContext(canvas);
+    const data = this.data.forecastData;
+    const r = 3;
+    const t = 50;
+    const step = 80;
+    let curX = 20;
+    const dY = textUp ? 10 : 0;
+    const points = [];
+    const textY = textUp ? -10 : 20
+
+    // 绘制线
+    ctx.setStrokeStyle(color);
+    ctx.beginPath();
+    for (let i = 0; i < data.length; i++) {
+      const y = +data[i][key] + dY;
+      if(i === 0) {
+        ctx.moveTo(curX, y);
+      } else {
+        ctx.lineTo(curX, y);
+      }
+      points.push([curX, y]);
+      curX += step;
+    }
+    ctx.stroke();
+    console.log(points)
+
+    // 绘制点
+    ctx.setFillStyle(color);
+    ctx.setLineWidth(5);
+    ctx.setStrokeStyle('#fff');
+    curX = 20;
+    for(let i = 0; i < points.length; i++) {
+      ctx.beginPath();
+      const item = points[i];
+      ctx.arc(item[0], item[1], r, 0, Math.PI * 2);
+      ctx.setFontSize(14);
+      ctx.fillText(`${item[1] - dY}°`, curX - 8, item[1] + textY);
+      ctx.stroke();
+      ctx.fill();
+      curX += step;
+    }
+
+    const canvasW = points[points.length-1][0] - points[0][0];
+    // const canvasH = points[points.length - 1][1] - points[0][1];
+    this.setData({
+      cWidth: canvasW + 40,
+      cHeight: 50
+    })
+    // console.log(canvasW, canvasH)
+    ctx.draw();
   }
 })
